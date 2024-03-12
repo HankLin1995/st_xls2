de57723dd17ace455f7806df515bb7451b61c867
YLIANET\zong-han01273 <apple84026113@gmail.com>
2024-03-12 16:25:54 +0800

    Init

---
+++ streamlit_app.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import streamlit as st
+import openpyxl
+import os
+
+# Streamlit App
+def main():
+    st.title('Excel 平均值計算和下載')
+
+    template_file = 'template/sample.xlsx'
+
+    workbook = openpyxl.load_workbook(template_file)
+
+    # 取得第一個工作表
+    sheet = workbook.worksheets[0]
+
+    # 設定 sheet 工作表 A2 儲存格內容為 "Test Excel"
+    sheet['A2'] = 'Test Excel.'
+
+    # 保存文件到临时目录
+    output_file = 'test.xlsx'
+    workbook.save(output_file)
+
+    # 提供下载链接给用户
+    st.write('点击下面的按钮下载处理后的 Excel 文件:')
+    with open(output_file, 'rb') as f:
+        bytes_data = f.read()
+    st.download_button(label='下载文件', data=bytes_data, file_name=output_file)
+
+    
+
+# 启动应用
+if __name__ == "__main__":
+    main()
