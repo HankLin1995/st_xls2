0d24520d343922861ff75832c83f1e35d3814ffe
YLIANET\zong-han01273 <apple84026113@gmail.com>
2024-03-27 14:47:40 +0800

    use .md to show the process in SP

---
+++ md/SP.md
@@ -0,0 +1,75 @@
+#### 二.基本參數設定
+
+φ = {phi}°
+
+主動土壓力係數（Ka）的計算公式如下：
+
+$$ K_a = \frac{{1 - \sin(\phi)}}{{1 + \sin(\phi)}} $$
+
+Ka = {Ka}
+
+被動土壓力係數（Kp）的計算公式如下：
+
+$$ K_p = \frac{{1 + \sin(\phi)}}{{1 - \sin(\phi)}} $$
+
+Kp = {Kp}
+
+---
+
+#### 三.O點上部計算
+
+(1) 假設 O 點為不動點，將擋土牆分為上下兩部分。下圖之牆後被動
+土壓力與牆前主動土壓力的差值，以一集中力 R 作用於上圖 O 點。
+
+(2) 對上圖之 O 點取彎矩平衡，取適當的彎矩安全係數 FS，可求得 d0；
+取水平力平衡，可得 R 值。
+
+$$ P_A = \frac{{1}}{{2}} \cdot \gamma \cdot (H + d_0)^2 \cdot K_a  $$
+
+$$ P_p = \frac{{1}}{{2}} \cdot \gamma  \cdot (d_0)^2 \cdot K_p $$
+
+$$ L_a=\frac{{(H + d_0)}}{{3}} $$
+
+$$ L_p=\frac{{d_0}}{{3}} $$
+
+$$ M_A = P_A \cdot L_a= \frac{{1}}{{2}} \cdot \gamma \cdot (H + d_0)^2 \cdot K_a \cdot \frac{{(H + d_0)}}{{3}}  $$  
+
+$$ M_p = P_p \cdot L_p = \frac{{1}}{{2}} \cdot \gamma \cdot d_0^2 \cdot K_p \cdot \frac{{d_0}}{{3}} $$
+
+$$ FS = \frac{{M_p}}{{M_A}} $$
+
+**對上圖之 O 點取彎矩平衡，FS按規範取1.5。**
+
+計算得到的d0為2：{d0_sol}M
+
+**對上圖取力平衡**
+
+$$ R = P_p- P_A $$
+
+計算得到的R值為：{R_value}
+
+---
+
+#### 四.O點下部計算
+
+**計算目的:至少須滿足  S >= R**
+
+根據經驗  D=1.1~1.2d0
+
+$$ P_{{Al}} = \frac{{1}}{{2}}(\gamma \cdot d_0 \cdot K_a + \gamma \cdot D \cdot K_a)(D - d_0) $$
+
+$$ P_{{pl}} = \frac{{1}}{{2}}(\gamma \cdot (H + d_0) \cdot K_p + \gamma \cdot (H + D) \cdot K_p)(D - d_0) $$
+
+$$ S = P_{{pl}}- P_{{Al}} $$
+
+當i={i_value}時，S={S_test_value}
+
+此時滿足  S >= R，試誤法求解得D=**{D}**
+
+---
+
+#### 五.計算成果
+
+計算所得之H+D={H_D}M
+
+#### 建議採用懸臂式板樁長度為 *{H_D_ceil}* M
--- page_Foundation.py
+++ page_Foundation.py
@@ -87,70 +87,93 @@ def SP():
 
         #說明開始
 
-        st.write(" #### 1.規範依據")
+        # 读取 Markdown 文件内容
+        with open(".\md\SP.md", "r", encoding="utf-8") as file:
+            markdown_text = file.read()
+
+        # 使用字符串格式化将变量值填充到 Markdown 文本中
+        markdown_text = markdown_text.format(
+            phi=phi,
+            Ka=Ka,
+            Kp=Kp,
+            d0_sol=d0_sol,
+            R_value=R_value,
+            i_value=round(i_value, 2),
+            S_test_value=S_test_value, 
+            D=i_value * d0_sol,
+            H_D=H + D,
+            H_D_ceil=math.ceil(H + D)
+        )
+
+
+        st.write(" #### 一、計算依據")
         url = "https://www.nlma.gov.tw/filesys/file/EMMA/L1120811.pdf"
         st.write(":clipboard:內政部建築物基礎構造設計規範(112年)--7.5.3")
         st.write(":link:點我查看[PDF](%s)" % url)
-        #基本參數設定
 
-        st.divider()
-        st.write("#### 2.基本參數")
+        # 在 Streamlit 中呈现 Markdown 内容
+        st.markdown(markdown_text)
+
+        # st.divider()
+        # st.write("#### 2.基本參數")
         
-        st.latex(r"\phi = " + str(phi) + r"^\circ")
+        # st.latex(r"\phi = " + str(phi) + r"^\circ")
 
-        st.write("主動土壓力係數（Ka）的計算公式如下：")
-        st.latex(r"K_a = \frac{{1 - \sin(\phi)}}{{1 + \sin(\phi)}}")
-        st.write("Ka=", Ka)
+        # st.write("主動土壓力係數（Ka）的計算公式如下：")
+        # st.latex(r"K_a = \frac{{1 - \sin(\phi)}}{{1 + \sin(\phi)}}")
+        # st.write("Ka=", Ka)
 
-        st.write("被動土壓力係數（Kp）的計算公式如下：")
-        st.latex(r"K_p = \frac{{1 + \sin(\phi)}}{{1 - \sin(\phi)}}")
-        st.write("Kp=", Kp)
+        # st.write("被動土壓力係數（Kp）的計算公式如下：")
+        # st.latex(r"K_p = \frac{{1 + \sin(\phi)}}{{1 - \sin(\phi)}}")
+        # st.write("Kp=", Kp)
 
-        st.divider()
+        # st.divider()
 
-        #計算第一階段
+        # #計算第一階段
 
-        st.write("#### 3.第一階段(上部)計算")
+        # st.write("#### 3.第一階段(上部)計算")
 
-        st.write("""(1) 假設 O 點為不動點，將擋土牆分為上下兩部分。下圖之牆後被動
-                                土壓力與牆前主動土壓力的差值，以一集中力 R 作用於上圖 O 點。
-                                \n(2) 對上圖之 O 點取彎矩平衡，取適當的彎矩安全係數 FS，可求得 d0；
-                                取水平力平衡，可得 R 值。""")
+        # st.write("""(1) 假設 O 點為不動點，將擋土牆分為上下兩部分。下圖之牆後被動
+        #                         土壓力與牆前主動土壓力的差值，以一集中力 R 作用於上圖 O 點。
+        #                         \n(2) 對上圖之 O 點取彎矩平衡，取適當的彎矩安全係數 FS，可求得 d0；
+        #                         取水平力平衡，可得 R 值。""")
 
-        st.latex(r"P_A = \frac{1}{2} \cdot \gamma \cdot (H + d_0)^2 \cdot K_a")
-        st.latex(r"P_p = \frac{1}{2} \cdot \gamma  \cdot (d_0)^2 \cdot K_p")
-        st.latex(r"R = P_p- P_A")
+        # st.latex(r"P_A = \frac{1}{2} \cdot \gamma \cdot (H + d_0)^2 \cdot K_a")
+        # st.latex(r"P_p = \frac{1}{2} \cdot \gamma  \cdot (d_0)^2 \cdot K_p")
+        # st.latex(r"R = P_p- P_A")
 
-        st.write("**對上圖之 O 點取彎矩平衡，FS放置於右側彎矩。**")
+        # st.write("**對上圖之 O 點取彎矩平衡，FS=抵抗彎矩(Mp)/驅動彎矩(MA)。**")
 
-        st.write("右側彎矩計算式=")
-        st.latex(r"M_r = \frac{1}{2} \cdot r \cdot (H + d_0)^2 \cdot K_a \cdot \frac{(H + d_0)}{3}")    
-        st.write("左側彎矩計算式=")
-        st.latex(r"M_l = \frac{1}{2} \cdot r \cdot d_0^2 \cdot K_p \cdot \frac{d_0}{3}")
+        # st.latex(r"L_a=\frac{(H + d_0)}{3}")
+        # st.latex(r"L_p=\frac{d_0}{3}")
 
-        st.write("解出的d0：",  d0_sol,"M")
-        st.write("**對上圖取力平衡**")
+        # st.latex(r"M_A = P_A \cdot L_a= \frac{1}{2} \cdot r \cdot (H + d_0)^2 \cdot K_a \cdot \frac{(H + d_0)}{3}......\tag{1}")    
+        # st.latex(r"M_p =P_p \cdot L_p= \frac{1}{2} \cdot r \cdot d_0^2 \cdot K_p \cdot \frac{d_0}{3}")
+        # st.latex(r"FS=M_p/M_A"+",FS大約等於1.5")
 
-        # 显示结果
-        st.write("計算得到的R值為:", R_value)
+        # st.write("解出的d0：",  d0_sol,"M")
+        # st.write("**對上圖取力平衡**")
 
-        #計算第二階段
-        st.divider()
-        st.write("#### 4.第二階段(下部)計算")
-        st.write("**計算目的:至少須滿足  S>=R**")
-        st.write("根據經驗  D=1.1~1.2d0")
+        # # 显示结果
+        # st.write("計算得到的R值為:", R_value)
 
-        st.latex(r"P_{\text{Al}} = \frac{1}{2}(r \cdot d_0 \cdot K_a + r \cdot D \cdot K_a)(D - d_0)")
-        st.latex(r"P_{\text{pl}} = \frac{1}{2}(r \cdot (H + d_0) \cdot K_p + r \cdot (H + D) \cdot K_p))(D - d_0)")
-        st.latex(r"S = P_{\text{pl}}- P_{\text{Al}}")
+        # #計算第二階段
+        # st.divider()
+        # st.write("#### 4.第二階段(下部)計算")
+        # st.write("**計算目的:至少須滿足  S>=R**")
+        # st.write("根據經驗  D=1.1~1.2d0")
 
-        st.write("當i=",round(i_value,2),"時，S=",S_test_value)
-        st.write("此時滿足  S>=R，則D=", i_value*d0_sol)
+        # st.latex(r"P_{\text{Al}} = \frac{1}{2}(r \cdot d_0 \cdot K_a + r \cdot D \cdot K_a)(D - d_0)")
+        # st.latex(r"P_{\text{pl}} = \frac{1}{2}(r \cdot (H + d_0) \cdot K_p + r \cdot (H + D) \cdot K_p))(D - d_0)")
+        # st.latex(r"S = P_{\text{pl}}- P_{\text{Al}}")
 
-        st.divider()
-        st.write("#### 5.計算成果")
-        st.write("計算所得之H+D=",H+D,"M")
-        st.write(" ##### :heavy_check_mark: 建議採用懸臂式板樁長度為",math.ceil(H+D),"M")
+        # st.write("當i=",round(i_value,2),"時，S=",S_test_value)
+        # st.write("此時滿足  S>=R，則D=", i_value*d0_sol)
+
+        # st.divider()
+        # st.write("#### 5.計算成果")
+        # st.write("計算所得之H+D=",H+D,"M")
+        # st.write(" ##### :heavy_check_mark: 建議採用懸臂式板樁長度為",math.ceil(H+D),"M")
         st.session_state.current_page = 'SP'
         
     st.session_state.current_page = 'SP'
